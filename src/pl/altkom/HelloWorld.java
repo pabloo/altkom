ecf0fdf06c67a374afa40cb90cff57ec1588c669
pabloo <47400raciborz>
2014-07-03 14:04:11 +0200

    dasada

--- src/pl/altkom/HelloWorld.java
+++ src/pl/altkom/HelloWorld.java
@@ -2,10 +2,11 @@ package pl.altkom;
 
 public class HelloWorld {
 public void showInfo() {
-	System.out.println("Hello World!!!");
+	System.out.println("Hello World ze zmianami!!!");
 }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// zmieniony plik
 
 	}
 
