9bb4e24af7565fa5e5afb00cc3856c1606197267
pabloo <47400raciborz>
2014-07-03 14:05:15 +0200

    gggg

--- src/pl/altkom/HelloWorld.java
+++ src/pl/altkom/HelloWorld.java
@@ -1,9 +1,10 @@
 package pl.altkom;
 
 public class HelloWorld {
-public void showInfo() {
-	System.out.println("Hello World ze zmianami!!!");
-}
+	public void showInfo() {
+		System.out.println("Hello World ze zmianami!!!");
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// zmieniony plik
