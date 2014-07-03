eabb8c355afc8dee3306fb7930da28f0d801c7b4
BeataD <b.dudek89@gmail.com>
2014-07-03 14:01:08 +0200

    dddd

--- src/pl/altkom/New.java
+++ src/pl/altkom/New.java
@@ -4,7 +4,7 @@ public class New {
 
 	public void showInfo (){
 		System.out.println("Pan kotek by chory i leżal w lozeczku i przyszedl pan doktor: jak sie masz koteczku");
-   //  fsddafasdfasdfasdfasd
+   //  fsddafasdfasdfasdfasd fasdf asdf asdf asd
 	}
 
 }
