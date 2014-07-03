3895a99bc4a1d799a12e2d28c9a182af55d3a5b2
BeataD <b.dudek89@gmail.com>
2014-07-03 13:56:49 +0200

    ver4

--- src/pl/altkom/New.java
+++ src/pl/altkom/New.java
@@ -3,7 +3,7 @@ package pl.altkom;
 public class New {
 
 	public void showInfo (){
-		System.out.println("Pan kotek by chory i leżal w lozeczku i przyszedl pan doktor jak sie masz koteczku");
+		System.out.println("Pan kotek by chory i leżal w lozeczku i przyszedl pan doktor: jak sie masz koteczku");
 
 	}
 
