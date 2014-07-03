8668b8d47c3563923235cda33f5c371160cde5c1
pabloo <47400raciborz>
2014-07-03 12:00:40 +0200

    HelloWorld v.2

---
+++ src/pl/altkom/HelloWorld.java
@@ -0,0 +1,12 @@
+package pl.altkom;
+
+public class HelloWorld {
+public void showInfo() {
+	System.out.println("Hello World!!!");
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
